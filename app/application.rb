e61a48d880b7d81267faebdc28bf9b7d89751d0a
Richard Melchior <raustin.melchior@gmail.com>
2018-06-16 06:43:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,9 @@ class Application
     resp = Rack::Response.new
     #time = Kernel.rand(1..24)
 
-    resp.write "#{time}\n"
-
     time = Time.new
+
+    resp.write "#{time}\n"
 
     resp.write "#{time}\n"
 
